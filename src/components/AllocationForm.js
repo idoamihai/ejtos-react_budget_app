530fcde32429f080b854b87e0c19f5301164dc72
ido amihai <idoamihai@gmail.com>
2023-01-27 18:35:49 -0500

    on task 6

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -56,15 +56,17 @@ const AllocationForm = (props) => {
                 </div>
                   <select className="custom-select" id="inputGroupSelect02" onChange={(event) => setAction(event.target.value)}>
                         <option defaultValue value="Add" name="Add">Add</option>
-                <option value="Reduce" name="Reduce">Reduce</option>
-                  </select>
-
+                <option value="Reduce" name="Reduce">Reduce</option> 
+                  </select> 
+                  <div style={{ marginLeft: '2rem' , size: 10}}>
+                    <p>£</p>
+                    </div>        
                     <input
                         required='required'
                         type='number'
                         id='cost'
                         value={cost}
-                        style={{ marginLeft: '2rem' , size: 10}}
+                        style={{ marginLeft: '.5rem' , size: 10}}
                         onChange={(event) => setCost(event.target.value)}>
                         </input>
 
