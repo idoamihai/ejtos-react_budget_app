a8ba13acb11e337a77f766242eeb16853ca0674c
ido amihai <idoamihai@gmail.com>
2023-01-30 11:36:32 -0500

    up to task 7

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -22,7 +22,7 @@ const Budget = () => {
 	
 	return (
 		<div className='alert alert-secondary'>
-            <span>Budget: £</span>
+            <span>Budget: $</span>
 			<input type="number" step="10" value={budget} onInput={(event)=>changeBudget(event.target.value)}></input>
 		</div>
 	);
