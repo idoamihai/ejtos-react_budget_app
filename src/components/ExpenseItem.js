e524f099734416da209b5181200bd1c98467b2b7
idoamihai <idoamihai@gmail.com>
2023-01-31 08:04:42 +0100

    Update ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,9 @@
 import React, { useContext } from 'react';
 import { TiDelete } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
+import { FcPlus,FcMinus } from 'react-icons/fc';
+
+const style = { color: "white", fontSize: "1.5em" }
 
 const ExpenseItem = (props) => {
     const { dispatch,currency } = useContext(AppContext);
@@ -43,8 +46,8 @@ const ExpenseItem = (props) => {
         <tr>
         <td>{props.name}</td>
         <td>{currency}{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
-        <td><button onClick={event=> decreaseAllocation(props.name)}>-</button></td>
+        <td><button onClick={event=> increaseAllocation(props.name)}><FcPlus style={style}/></button></td>
+        <td><button onClick={event=> decreaseAllocation(props.name)}><FcMinus/></button></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
